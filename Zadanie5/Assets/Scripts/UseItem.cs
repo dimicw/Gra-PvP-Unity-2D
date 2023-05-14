47b2658c81857e03c592c6390a04796a6acc84f1
dimicw <104204285+dimicw@users.noreply.github.com>
2023-05-14 15:24:42 +0200

    map and proper potions

--- Zadanie5/Assets/Scripts/UseItem.cs
+++ Zadanie5/Assets/Scripts/UseItem.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class UseItem : MonoBehaviour
 {
     [SerializeField] bool Destroyable = false;
+    private bool alreadyUsed = false;
     public Animator animator;
 
     public void Activate()
     {
         animator.SetBool("Used", true);
-        if (Destroyable)
-            this.GetComponent<CircleCollider2D>().isTrigger = true;
-        if (transform.GetChild(0) != null)
-            transform.GetChild(0).gameObject.SetActive(true);
+        if (!alreadyUsed)
+        {
+            if (Destroyable)
+                this.GetComponent<CircleCollider2D>().isTrigger = true;
+            if (transform.GetChild(0) != null)
+                transform.GetChild(0).gameObject.SetActive(true);
+            alreadyUsed = true;
+        }
     }
 }
